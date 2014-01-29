4939f86feff31b402ee9d8bbef6e0d761737596f
noxan <noxan@byteweaver.net>
2014-01-29 21:02:13 +0100

    implement path handling in core file and add some basic tests

---
+++ filemanager/core.py
@@ -0,0 +1,28 @@
+import os
+
+from django.conf import settings
+
+from filemanager.settings import DIRECTORY
+
+
+class Filemanager(object):
+    def __init__(self, path=None):
+        self.update_path(path)
+
+    def update_path(self, path):
+        if path is None or len(path) == 0:
+            self.path = ''
+            self.abspath = DIRECTORY
+        else:
+            self.path = self.validate_path(path)
+            self.abspath = os.path.join(DIRECTORY, self.path)
+        self.location = os.path.join(settings.MEDIA_ROOT, self.abspath)
+        self.url = os.path.join(settings.MEDIA_URL, self.abspath)
+
+    def validate_path(self, path):
+        # replace backslash with slash
+        path = path.replace('\\', '/')
+        # remove leading and trailing slashes
+        path = '/'.join([i for i in path.split('/') if i])
+
+        return path
---
+++ filemanager/tests/test_core.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from filemanager.core import Filemanager
+
+
+class FilemanagerTest(TestCase):
+    def setUp(self):
+        self.fm = Filemanager()
+
+    def test_basic_path(self):
+        self.assertEqual(self.fm.path, '')
+        self.assertEqual(self.fm.abspath, 'uploads')
+
+    def test_different_path(self):
+        self.fm.update_path('another/folder/')
+        self.assertEqual(self.fm.path, 'another/folder')
+        self.assertEqual(self.fm.abspath, 'uploads/another/folder')
+
+    def test_path_from_root(self):
+        self.fm.update_path('/folder/')
+        self.assertEqual(self.fm.path, 'folder')
+        self.assertEqual(self.fm.abspath, 'uploads/folder')
