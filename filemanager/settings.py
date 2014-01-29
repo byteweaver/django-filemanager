91d4a994ab2a7ce0fdd2e4a24ba7d25b9d273936
noxan <noxan@byteweaver.net>
2014-01-29 21:01:26 +0100

    add directory (maybe replace dedicated url and root) setting

--- filemanager/settings.py
+++ filemanager/settings.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
 
+DIRECTORY = getattr(settings, 'FILEMANAGER_DIRECTORY', 'uploads')
+
 MEDIA_ROOT = getattr(settings, "FILEMANAGER_MEDIA_ROOT", os.path.join(settings.MEDIA_ROOT, 'uploads'))
 
 MEDIA_URL = getattr(settings, "FILEMANAGER_MEDIA_URL", os.path.join(settings.MEDIA_URL, 'uploads'))
