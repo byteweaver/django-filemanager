360cd9e2a8c1b0af68083c7f07d99c67da72a04a
noxan <noxan@byteweaver.net>
2014-01-29 23:05:11 +0100

    use directory setting for media root and url settings

--- filemanager/settings.py
+++ filemanager/settings.py
@@ -6,8 +6,8 @@ from django.core.files.storage import FileSystemStorage
 
 DIRECTORY = getattr(settings, 'FILEMANAGER_DIRECTORY', 'uploads')
 
-MEDIA_ROOT = getattr(settings, "FILEMANAGER_MEDIA_ROOT", os.path.join(settings.MEDIA_ROOT, 'uploads'))
+MEDIA_ROOT = getattr(settings, "FILEMANAGER_MEDIA_ROOT", os.path.join(settings.MEDIA_ROOT, DIRECTORY))
 
-MEDIA_URL = getattr(settings, "FILEMANAGER_MEDIA_URL", os.path.join(settings.MEDIA_URL, 'uploads'))
+MEDIA_URL = getattr(settings, "FILEMANAGER_MEDIA_URL", os.path.join(settings.MEDIA_URL, DIRECTORY))
 
 STORAGE = getattr(settings, "FILEMANAGER_STORAGE", FileSystemStorage(location=MEDIA_ROOT, base_url=MEDIA_URL))
